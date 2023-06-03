19912b191dddec62632efd99649396a45e174bac
HalianRC <125613474+HalianRC@users.noreply.github.com>
2023-06-03 17:54:45 -0300

    control-balance

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,49 @@
 /* Desenvolva sua lógica aqui */
 import { valuesCategory, insertedValues } from "./valuesData.js";
+
+function renderCards(insertedValues){
+  const sectionThird = document.querySelector(".section__third")
+
+  for(let i = 0; i<insertedValues.length; i++){
+    const value = insertedValues[i]
+
+    const cardsContainer = document.createElement("div")
+    const divCard = document.createElement("div")
+    const cardTitle = document.createElement("p")
+    const buttonWithImage = document.createElement("div")
+    const cardButton = document.createElement("button")
+    const cardImage = document.createElement("figure")
+    const imageTrash = document.createElement("img")
+    
+    cardTitle.innerText = value.value
+    imageTrash.src = "./src/assets/trash.svg"
+
+
+    cardsContainer.classList.add("cards__container")
+    divCard.classList.add("div-card")
+    cardTitle.classList.add("card-title")
+    buttonWithImage.classList.add("button-with-image")
+    cardButton.classList.add("card-button")
+    cardImage.classList.add("card-image")
+
+    sectionThird.appendChild(cardsContainer)
+    cardsContainer.appendChild(divCard)
+    divCard.append(cardTitle,buttonWithImage)
+    buttonWithImage.append(cardButton, cardImage)
+    cardImage.appendChild(imageTrash)
+
+  }
+}
+renderCards(insertedValues)
+
+// <section class="section__third">
+// <div class="div-card">
+//   <p class="card-title">R$ 40,00</p>
+//   <div class="button-with-image" >
+//     <button class="card-button">Entrada</button>
+//     <figure class="card-image">
+//       <img src="./src/assets/trash.svg" alt="trash">
+//     </figure>
+//   </div>
+// </div>
+// </section>
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,22 @@
 /* Desenvolva sua lógica aqui */
+function openModal(){
+  const modalContainer = document.querySelector('.modal__container')
+  const buttonRegister = document.querySelector('.button-register')
+
+  buttonRegister.addEventListener("click", function(e){
+    e.preventDefault()
+    modalContainer.showModal()
+  })
+}
+openModal()
+
+function closeModal(){
+  const modalClose = document.querySelector(".modal-close")
+  const modalContainer = document.querySelector('.modal__container')
+
+  modalClose.addEventListener("click", function(e){
+    e.preventDefault()
+    modalContainer.close()
+  })
+}
+closeModal()
